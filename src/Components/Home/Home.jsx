4da97fb8196b616c8c0fe7f52f50ff9dacbec741
bilalmohamed1298 <101932528+bilalmohamed1298@users.noreply.github.com>
2024-12-26 22:17:43 +0200

    updated content

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Home() {
-    return (
-        <div>
-            
+  let [trendingMovies, setTrendingMovies] = useState([]);
+  let [trendingSeries, setTrendingSeries] = useState([]);
+
+  let getApi = async () => {
+    let moviesApi = await axios.get(
+      "https://api.themoviedb.org/3/trending/movie/day?api_key=6b89ab44f20e0ea3a35ec5d5e7146bf4"
+    );
+    let seriesApi = await axios.get(
+      "https://api.themoviedb.org/3/trending/tv/day?api_key=6b89ab44f20e0ea3a35ec5d5e7146bf4"
+    );
+    setTrendingSeries(seriesApi.data.results)
+    setTrendingMovies(moviesApi.data.results);
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  console.log(trendingMovies);
+  console.log(trendingSeries);
+  var imgURL = "https://image.tmdb.org/t/p/original/";
+
+  return (
+    <>
+      <div className="container mt-5 text-white">
+        <div className="row g-4 mt-5">
+          <div className="col-md-4 align-content-center">
+            <h1 className="">Trending Movies to watch now</h1>
+            <p className="fw-lighter">Best trending movies in the last day</p>
+          </div>
+
+          {trendingMovies.map((movie,index) =>
+            index < 5 ? (
+              <div className="col-md-4">
+                <img
+                  src={imgURL + movie.backdrop_path}
+                  alt=""
+                  className="w-100 rounded-3"
+                />
+                <h6 className="mt-3">{movie.title}</h6>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+          
         </div>
-    )
+
+
+
+        <div className="row g-4 mt-5">
+          <div className="col-md-4 align-content-center">
+            <h1 className="">Trending TV Shows to watch now</h1>
+            <p className=" fw-lighter">Best trending movies in the last day</p>
+          </div>
+          {trendingSeries.map((serie,index) =>
+            index < 5 ? (
+              <div className="col-md-4 ">
+                <img
+                  src={imgURL + serie.backdrop_path}
+                  alt=""
+                  className="w-100 rounded-3"
+                />
+                <h6 className="mt-3">{serie.name}</h6>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,82 +3,162 @@ import { Link } from "react-router";
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-sm bg-success">
-        <div className="container">
-        <Link className="navbar-brand text-white fw-bolder me-4 logo" to="home">
-          Etfarag
-        </Link>
-        <button
-          className="navbar-toggler d-lg-none"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        ></button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-          <ul className="navbar-nav me-3 mt-2 mt-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger active" to="home">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="movies">
-                Movies
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="tv">
-                Tv Shows
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="people">
-                People
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="networks">
-                Networks
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex my-2 me-auto my-lg-0">
-            <input
-              className="form-control me-sm-2 rounded-4"
-              type="text"
-              placeholder="Search"
-            />
-            <button
-              className="btn btn-outline-light my-2 my-sm-0 rounded-4"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
-          <ul className="navbar-nav mt-2 mt-lg-0">
-          <li className="nav-item">
-              <Link className="nav-link me-3 text-white txtbiger" to="signin">
-                Sign In
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white txtbiger" to="signup">
-                Sign Up
-              </Link>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-xl">
+          <Link
+            className="navbar-brand txt-color fw-bolder me-4 logo"
+            to="home"
+          >
+            Etfarag
+          </Link>
+          <button
+            className="navbar-toggler txt-color"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-3 mt-2 mt-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger active" to="home">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="movies">
+                  Movies
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="tv">
+                  Tv Shows
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="people">
+                  People
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="networks">
+                  Networks
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex me-auto" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success txt-color" type="submit">
+                Search
+              </button>
+            </form>
+            <ul className="navbar-nav mt-2  mt-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link me-3 text-white txtbiger" to="signin">
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="signup">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        </div>
-
       </nav>
+      {/* <nav className="navbar navbar-expand-sm">
+        <div className="container">
+          <Link
+            className="navbar-brand txt-color fw-bolder me-4 logo"
+            to="home"
+          >
+            Etfarag
+          </Link>
+          <button
+            className="navbar-toggler d-lg-none"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          ></button>
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav me-3 mt-2 mt-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger active" to="home">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="movies">
+                  Movies
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="tv">
+                  Tv Shows
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="people">
+                  People
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="networks">
+                  Networks
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex my-2 me-auto my-lg-0">
+              <input
+                className="form-control me-sm-2 rounded-4"
+                type="text"
+                placeholder="Search"
+              />
+              <button
+                className="btn btn-outline-success txt-color my-2 my-sm-0 rounded-4"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+            <ul className="navbar-nav mt-2 mt-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link me-3 text-white txtbiger" to="signin">
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-white txtbiger" to="signup">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> */}
     </div>
   );
 }
