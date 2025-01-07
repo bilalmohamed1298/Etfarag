0fe3955c57fd9a336dfb81ef9369cb36a512ac10
bilalmohamed1298 <101932528+bilalmohamed1298@users.noreply.github.com>
2025-01-07 11:46:39 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,26 +5,29 @@ function Navbar(props) {
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-xl">
-          <Link
-            className="navbar-brand txt-color fw-bolder me-4 logo"
-            to="home"
-          >
-            Etfarag
-          </Link>
-          <button
-            className="navbar-toggler text-primary bc opacity-75"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {props.userData ? (
-              <>
+          {props.userData ? (
+            <>
+              <Link
+                className="navbar-brand txt-color fw-bolder me-4 logo"
+                to="home"
+              >
+                Etfarag
+              </Link>
+              <button
+                className="navbar-toggler text-primary bc opacity-75"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
                 <ul className="navbar-nav me-3 mt-2 mt-lg-0">
                   <li className="nav-item">
                     <Link
@@ -69,12 +72,6 @@ function Navbar(props) {
                     Search
                   </button>
                 </form>
-              </>
-            ) : (
-              ""
-            )}
-            {props.userData ? (
-              <>
                 <Link
                   onClick={props.removeUserData}
                   className="nav-link me-3 text-white txtbiger"
@@ -82,107 +79,13 @@ function Navbar(props) {
                 >
                   Sign Out
                 </Link>
-              </>
-            ) : (
-              <>
-                {/* <ul className="navbar-nav mt-2  mt-lg-0 ms-auto">
-                  <li className="nav-item">
-                    <Link
-                      className="nav-link me-3 text-white txtbiger"
-                      to="signin"
-                    >
-                      Sign In
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link text-white txtbiger" to="signup">
-                      Sign Up
-                    </Link>
-                  </li>
-                </ul> */}
-              </>
-            )}
-          </div>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </nav>
-      {/* <nav className="navbar navbar-expand-sm">
-        <div className="container">
-          <Link
-            className="navbar-brand txt-color fw-bolder me-4 logo"
-            to="home"
-          >
-            Etfarag
-          </Link>
-          <button
-            className="navbar-toggler d-lg-none"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          ></button>
-          <div className="collapse navbar-collapse" id="collapsibleNavId">
-            <ul className="navbar-nav me-3 mt-2 mt-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger active" to="home">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="movies">
-                  Movies
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="tv">
-                  Tv Shows
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="people">
-                  People
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="networks">
-                  Networks
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex my-2 me-auto my-lg-0">
-              <input
-                className="form-control me-sm-2 rounded-4"
-                type="text"
-                placeholder="Search"
-              />
-              <button
-                className="btn btn-outline-success txt-color my-2 my-sm-0 rounded-4"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
-            <ul className="navbar-nav mt-2 mt-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link me-3 text-white txtbiger" to="signin">
-                  Sign In
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white txtbiger" to="signup">
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
     </div>
   );
 }
